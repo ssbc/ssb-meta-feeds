1ed06afd3b94a3ec2ecf071ca052827b132aec64
Anders Rune Jensen <arj03@protonmail.ch>
2021-05-07 22:38:42 +0200

    Update to latest spec, add tombstone

--- metafeed.js
+++ metafeed.js
@@ -2,18 +2,34 @@ const ssbKeys = require('ssb-keys')
 
 // FIXME: define and use json schema
 
-exports.operation = function(operation, feedformat, feedpurpose,
-                             subfeedKey, metafeedKey) {
+exports.add = function(feedformat, feedpurpose, subfeedKey, metafeedKey) {
   const msg = {
-    type: 'metafeed/operation',
-    operation,
+    type: 'metafeed/add',
     feedformat,
     feedpurpose,
     subfeed: subfeedKey.id,
     metafeed: metafeedKey.id, 
-    nonce: Date.now()
+    nonce: Date.now(),
+    tangle: {
+      metafeed: { root: null, previous: null }
+    }
   }
   msg.subfeedSignature = ssbKeys.sign(subfeedKey, JSON.stringify(msg))
-  
+
+  return msg
+}
+
+exports.tombstone = function(subfeedKey, root, previous, reason) {
+  const msg = {
+    type: 'metafeed/tombstone',
+    subfeed: subfeedKey.id,
+    nonce: Date.now(),
+    reason,
+    tangle: {
+      metafeed: { root, previous }
+    }
+  }
+  msg.subfeedSignature = ssbKeys.sign(subfeedKey, JSON.stringify(msg))
+
   return msg
 }
--- test/metafeed.js
+++ test/metafeed.js
@@ -8,12 +8,33 @@ test('add a feed to metafeed', (t) => {
   const mfKey = keys.deriveFeedKeyFromSeed(seed, 'ssb-meta-feeds-v1:metafeed')
   const sfKey = keys.deriveFeedKeyFromSeed(seed, 'ssb-meta-feeds-v1:metafeed/main')
 
-  const msg = metafeed.operation('add', 'classic', 'main', sfKey, mfKey)
+  const msg = metafeed.add('classic', 'main', sfKey, mfKey)
   //console.log(msg)
   t.true(msg.subfeedSignature.endsWith(".sig.ed25519"), "correct signature format")
   t.equal(msg.subfeed, sfKey.id, "correct subfeed id")
   t.equal(msg.metafeed, mfKey.id, "correct metafeed id")
-  
+
+  t.end()
+})
+
+test('tombstone a feed in a metafeed', (t) => {
+  const seed_hex = '4e2ce5ca70cd12cc0cee0a5285b61fbc3b5f4042287858e613f9a8bf98a70d39'
+  seed = Buffer.from(seed_hex, 'hex')
+  const mfKey = keys.deriveFeedKeyFromSeed(seed, 'ssb-meta-feeds-v1:metafeed')
+  const sfKey = keys.deriveFeedKeyFromSeed(seed, 'ssb-meta-feeds-v1:metafeed/main')
+
+  const rootId = "%msofmfos"
+  const previousId = "%test"
+  const reason = 'Feed no longer used'
+
+  const msg = metafeed.tombstone(sfKey, rootId, previousId, reason)
+  //console.log(msg)
+  t.true(msg.subfeedSignature.endsWith(".sig.ed25519"), "correct signature format")
+  t.equal(msg.subfeed, sfKey.id, "correct subfeed id")
+  t.equal(msg.tangle.metafeed.root, rootId, "correct root")
+  t.equal(msg.tangle.metafeed.previous, previousId, "correct previous")
+  t.equal(msg.reason, reason, "correct reason")
+
   t.end()
 })
 
