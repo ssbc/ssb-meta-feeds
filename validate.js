ba23389ff6079050c8b5e7eb7b9f681771c04859
mycognosist <gnomad@cryptolab.net>
2021-08-06 15:37:57 +0200

    add signature verification

--- validate.js
+++ validate.js
@@ -1,12 +1,18 @@
+const bencode = require('bencode')
 const bfe = require('ssb-bfe')
+// perhaps verification functions should be added to `keys` module?
+const ssbKeys = require('ssb-keys')
+
+const CONTENT_SIG_PREFIX = Buffer.from('bendybutt', 'utf8')
 
 /**
  * Validate a single meta feed message.
  *
  * @param {Array | string} contentSection - an array of `content` and `contentSignature` or an encrypted string
+ * @param {Buffer | string | null} hmacKey - a valid HMAC key for signature verification
  * @returns {Object | true} an `Error` object or `true` in the case of successful validation
  */
-exports.validateSingle = function (contentSection) {
+exports.validateSingle = function (contentSection, hmacKey) {
   if (contentSection === null || contentSection === undefined)
     return new Error(
       `invalid message: contentSection cannot be null or undefined`
@@ -59,5 +65,65 @@ exports.validateSingle = function (contentSection) {
       )
   }
 
+  const signatureErr = validateSignature(
+    content.subfeed,
+    content,
+    contentSignature,
+    hmacKey
+  )
+  if (signatureErr) return signatureErr
+
   return true
+}
+
+/**
+ * Verify that the contentSignature correctly signs the content.
+ *
+ * @param {string} subfeedKey - `subfeed` key for the message
+ * @param {Buffer} content - Dictionary of meta feed metadata
+ * @param {string} contentSignature - Base64-encoded signature for the given `content`
+ * @param {Buffer | string | null} hmacKey - HMAC key that was used to sign the payload
+ * @returns {Object | undefined} Either an Error containing a message or an `undefined` value for successful verification
+ */
+function validateSignature(subfeedKey, content, contentSignature, hmacKey) {
+  const hmacKeyErr = validateHmacKey(hmacKey)
+  if (hmacKeyErr) return hmacKeyErr
+
+  if (
+    !ssbKeys.verify(
+      { public: subfeedKey, curve: 'ed25519' },
+      contentSignature,
+      hmacKey,
+      Buffer.concat([CONTENT_SIG_PREFIX, bencode.encode(content)])
+    )
+  )
+    return new Error(
+      `invalid message: contentSignature must correctly sign the content using the subfeed key; ${subfeedKey}`
+    )
+}
+
+/**
+ * Validate an HMAC key.
+ *
+ * @param {Buffer | string | null | undefined} hmacKey
+ * @returns {Object | boolean} Either an Error containing a message or a `false` value for successful validation
+ */
+function validateHmacKey(hmacKey) {
+  if (hmacKey === undefined || hmacKey === null) return false
+
+  const bytes = Buffer.isBuffer(hmacKey)
+    ? hmacKey
+    : Buffer.from(hmacKey, 'base64')
+
+  if (typeof hmacKey === 'string') {
+    if (bytes.toString('base64') !== hmacKey)
+      return new Error(
+        `invalid hmac key: "${hmacKey}", expected string to be base64 encoded`
+      )
+  }
+
+  if (bytes.length !== 32)
+    return new Error(
+      `invalid hmac key: "${hmacKey}" with length ${hmacKey.length}, expected 32 bytes`
+    )
 }
