7f8e07b02b58abfb57c74f4f8f57a603c403dae8
mixmix <mix@protozoa.nz>
2022-10-03 17:29:17 +1300

    remove un-needed test

--- test/api/find-or-create.test.js
+++ test/api/find-or-create.test.js
@@ -43,27 +43,3 @@ test('findOrCreate', (t) => {
     })
   })
 })
-
-test('findOrCreate (metadata.recps)', (t) => {
-  const sbot = Testbot()
-
-  const ownKey = Buffer.from(
-    '30720d8f9cbf37f6d7062826f6decac93e308060a8aaaa77e6a4747f40ee1a76',
-    'hex'
-  )
-  sbot.box2.setOwnDMKey(ownKey)
-
-  const details = {
-    feedpurpose: 'chess',
-    metadata: {
-      recps: [sbot.id]
-    }
-  }
-
-  sbot.metafeeds.findOrCreate(details, (err, chessF) => {
-    if (err) throw err
-
-    t.deepEqual(chessF.metadata.recps, [sbot.id], 'creates encrypted subfee')
-    sbot.close(true, t.end)
-  })
-})
