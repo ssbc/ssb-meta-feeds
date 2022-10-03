35fdce1f777773a52e209cca72d18a13f0931437
mixmix <mix@protozoa.nz>
2022-10-03 17:26:04 +1300

    get dat liscence

--- constants.js
+++ constants.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Mix Irving
+//
+// SPDX-License-Identifier: LGPL-3.0-only
+
 const BB1 = 'bendybutt-v1'
 const v1Details = { feedpurpose: 'v1', feedformat: BB1 }
 const NOT_METADATA = new Set([
